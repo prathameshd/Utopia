44b4cdc8e62193c87b3db658664b62fe43afbd73
Sai Rohith Achanta <sairohith.achanta@gmail.com>
2019-01-26 20:22:13 -0500

    4: Developed login API and returning JWT token in response
    4: Developed login API and returning JWT token in response

--- AuthServices/Services/service.py
+++ AuthServices/Services/service.py
@@ -40,3 +40,30 @@ class Service:
 	# Checkthe validity of the email
 	def is_valid_email(self, email):
 		return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+
+	# Service method to generate a JWT and provide to user
+	def login(self, data):
+		try:
+			mongo_config = MongoConfig()
+			collection = mongo_config.db()
+			if(collection):
+				user_obj = User()
+				search_user = {'email': data['email']}
+				user = collection.find(search_user)
+				crypto = Crypto()
+				if(user):
+					if(crypto.verify_decrypted_string(data['password'], user[0]['password'])):
+						user_obj.first_name = user[0]['firstName']
+						user_obj.last_name = user[0]['lastName']
+						user_obj.token = Jwt.encode_auth_token(user_id=user[0]['_id']).decode()
+						return user_obj
+					else:
+						return "Invalid Credentials"
+				else:
+					return "User not available"
+			else:
+				return "Unable to connect to database"
+		except IndexError as IE:
+			return "User not available"
+		except Exception as e:
+			raise e
