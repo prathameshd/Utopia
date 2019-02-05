90c745c367a49eb478c827f2af6ebc03b1757182
Sai Rohith Achanta <sairohith.achanta@gmail.com>
2019-02-05 18:53:44 -0500

    #28: Removed last name field in login APIx
    #28: Removed last name field in login APIx

--- AuthServices/Services/service.py
+++ AuthServices/Services/service.py
@@ -56,7 +56,7 @@ class Service:
 				if(user):
 					if(crypto.verify_decrypted_string(data['password'], user[0]['password'])):
 						user_obj.first_name = user[0]['firstName']
-						user_obj.last_name = user[0]['lastName']
+						#user_obj.last_name = user[0]['lastName']
 						user_obj.user_id = str(user[0]['_id'])
 						user_obj.token = Jwt.encode_auth_token(user_id=user[0]['_id']).decode()
 						return user_obj
