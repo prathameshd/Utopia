bd24e53a88440e29f32095490ee83e9a3f663425
Sai Rohith Achanta <sairohith.achanta@gmail.com>
2019-02-07 20:16:47 -0500

    #56 #57 #58 : Implemented logout functionality, toast message and restricted the access for urlsif user not logged in
    #56 #57 #58 : Implemented logout functionality, toast message and restricted the access for urlsif user not logged in

--- UI/client/src/components/auth/Register.js
+++ UI/client/src/components/auth/Register.js
@@ -1,93 +1,92 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
-
+import {ToastContainer, ToastStore} from 'react-toasts'
+var config = require('../../config');
 class Register extends Component {
-    constructor(){
+  constructor(){
 
-        super();
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            password2: '',
-            errors: {}
-        };
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-    
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    
-    
-    onChange(e){
-        this.setState({ [e.target.name]: e.target.value});
-    }
-    
-    onSubmit(e){
-        e.preventDefault();
-        
-        const newUser = {
-            firstName: this.state.name,
-            email: this.state.email,
-            password: this.state.password,
-            confirmPassword: this.state.password2
-        };
+  onChange(e){
+    this.setState({ [e.target.name]: e.target.value});
+  }
 
-              return axios
-                ({
-                  method:'post',
-                  url:'http://localhost:5000/register',
-                  headers: {'Access-Control-Allow-Origin': '*'},
-                  data: newUser
-                   
-                  })
-                  .then((response)=>{
-                      console.log(response)
-                    if(response.status == 200)
-                    {
-                      alert("Congrats! Your account has been created!")
-                     window.location = 'http://localhost:3000';
-                     
-                    }
-                  }).catch(err => 
-                 {  
-                    console.log("Error registering...!", err)
-                    return([])
-                  })
+  onSubmit(e){
+    e.preventDefault();
+
+    const newUser = {
+      firstName: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.password2
+    };
+
+    return axios
+    ({
+      method:'post',
+      url: config.authHost+'/register',
+      headers: {'Access-Control-Allow-Origin': '*'},
+      data: newUser
+
+    })
+    .then((response)=>{
+      console.log(response)
+      if(response.status == 200)
+      {
+        ToastStore.success("Congrats! Your account has been created!");
+        window.location = 'http://localhost:3000';
+
+      }
+    }).catch(err =>
+      {
+        console.log("Error registering...!", err)
+
+        return([])
+      })
 
     }
-    
+
     render(){
-        return(
-                  <div className="register">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-md-8 m-auto">
-                          <h1 className="display-4 text-center color-white">Sign Up</h1>
-                          <p className="lead text-center color-white" >Create your Utopia account</p>
-                          <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                              <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} required />
-                            </div>
-                            <div className="form-group">
-                              <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
-                              
-                            </div>
-                            <div className="form-group">
-                              <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
-                            </div>
-                            <div className="form-group">
-                              <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange}/>
-                            </div>
-                            <input type="submit"  className="btn btn-info btn-block mt-4" />
-                          </form>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-            );
-    }
-}
+      return(
+        <div className="register">
+        <div className="container">
+        <div className="row">
+        <div className="col-md-8 m-auto">
+        <h1 className="display-4 text-center color-white">Sign Up</h1>
+        <p className="lead text-center color-white" >Create your Utopia account</p>
+        <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+        <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} required />
+        </div>
+        <div className="form-group">
+        <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
 
-export default Register;
+        </div>
+        <div className="form-group">
+        <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+        </div>
+        <div className="form-group">
+        <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange}/>
+        </div>
+        <input type="submit"  className="btn btn-info btn-block mt-4" />
+        </form>
+        </div>
+        </div>
+        </div>
+        <ToastContainer position={ToastContainer.POSITION.BOTTOM_RIGHT} store={ToastStore}/>
+        </div>
+      );
+    }
+  }
+
+  export default Register;
--- UI/client/src/components/layout/Navbar.js
+++ UI/client/src/components/layout/Navbar.js
@@ -1,33 +1,73 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+var config = require('../../config');
 
 class Navbar extends Component {
-    render(){
-        return (
-            <div>
-              <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-                    <div className="container">
-                      <Link className="navbar-brand" to="/">Utopia</Link>
-                      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-                        <span className="navbar-toggler-icon"></span>
-                      </button>
-                
-                      <div className="collapse navbar-collapse" id="mobile-nav">
-                
-                        <ul className="navbar-nav ml-auto">
-                          <li className="nav-item">
-                            <Link className="nav-link" to="/register">Sign Up</Link>
-                          </li>
-                          <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                </nav> 
-            </div>    
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  logout(){
+    sessionStorage.removeItem("jwt");
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("spotifyToken");
+    window.location.href = "/login"
+  }
+
+  navigateProfileService(){
+
+  }
+
+  render(){
+    return (
+      <div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <div className="container">
+      <Link className="navbar-brand" to="/">Utopia</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+      <span className="navbar-toggler-icon"></span>
+      </button>
+      {
+        sessionStorage.getItem('jwt')!=null && sessionStorage.getItem('name')!=null?
+
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+        {sessionStorage.getItem('name')}
+        </DropdownToggle>
+        <DropdownMenu>
+        <DropdownItem onClick={this.navigateProfileService}>Profile Service</DropdownItem>
+        <DropdownItem onClick={this.logout}>Logout</DropdownItem>
+        </DropdownMenu>
+        </Dropdown>
+        :
+        <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+        <Link className="nav-link" to="/register">Sign Up</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link" to="/login">Login</Link>
+        </li>
+        </ul>
+        </div>
+      }
+      </div>
+      </nav>
+      </div>
+    )
+  }
 }
 
 export default Navbar;
