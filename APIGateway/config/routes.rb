9e1605ba003608143000c165a6ec42168bde4ef4
Sai Rohith Achanta <sairohith.achanta@gmail.com>
2019-02-26 18:42:52 -0500

    #82: Added Profile Service APIs to APIGateway
    #82: Added Profile Service APIs to APIGateway

--- APIGateway/config/routes.rb
+++ APIGateway/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  # Routes to APIs present in app/controller/profile_services_controller/
+  get 'profile_services/get_history_and_mood'
+  post 'profile_services/set_history_and_mood'
+  get 'profile_services/get_personal_details'
+
   # Routes to APIs present in app/controller/api_broker_controller/
   get 'api_broker/get_auth'
   post 'api_broker/get_access'
