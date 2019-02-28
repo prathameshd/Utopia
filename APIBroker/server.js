36d0e0720693e6eba61c5adcdf05a26808079063
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-02-28 18:49:39 -0500

    #93: Getting dynamic addresses

--- APIBroker/server.js
+++ APIBroker/server.js
@@ -9,8 +9,9 @@ var express = require('express'),
   port = process.env.PORT || 3002,
   bodyParser = require('body-parser');
 
-const cors = require('cors')
 
+const cors = require('cors')
+const axios = require("axios");
 
 // For parsing requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,21 +31,46 @@ routes(app);
 
 // ZooKeeper Service registration
 var zookeeper = require('node-zookeeper-client');
-var client = zookeeper.createClient('localhost:2181');
+var client = zookeeper.createClient('149.165.170.7:2181');
 var path = "/APIBroker";
-var data ={host: "localhost", port: "3002"}
-client.once('connected', function () {
+var hostAddress=" ";
+
+//getting the IP
+dynamicAddress=function(){
+    axios({
+      method:'get',
+      url: 'https://ip.42.pl/raw',
+    }).then((response)=>{
+      console.log("got the IP")
+       hostAddress=response.data
+       var data={host:hostAddress,port:'3002'}
+
+        client.once('connected', function () {
+    console.log(data)
     console.log('[APIBroker] Connected to ZOOKEEPER!');
  
     client.create(path, new Buffer(JSON.stringify(data)), function (error) {
-        if (error) {
+        if (error) 
+        {
             console.log('[APIBroker]  Failed to create node: %s due to: %s.', path, error);
-        } else {
+        }
+        else
+        {
             console.log('[APIBroker]  Node: %s is successfully created.', path);
         }
  
         client.close();
     });
-});
+    });
  
 client.connect();
+
+    }).catch((err)=>{
+      res.status(400).json(utils.custom_JSON_formatter('error',err))
+    });
+    return hostAddress
+    
+}
+var temp;
+temp=dynamicAddress();
+
--- ProfileServices/server.js
+++ ProfileServices/server.js
@@ -9,6 +9,12 @@ var express = require('express'),
   port = process.env.PORT || 3001,
   bodyParser = require('body-parser');
 
+// ZooKeeper Service registration
+var zookeeper = require('node-zookeeper-client');
+var client = zookeeper.createClient('149.165.170.7:2181');
+var path = "/ProfileServices";
+
+
 // Mongo client
 var MongoClient = require('mongodb').MongoClient;
 
@@ -29,6 +35,8 @@ MongoClient.connect(MONGO_STRING, function(err, client) {
 });
 
 const cors = require('cors')
+const axios = require("axios");
+var hostAddress="";
 
 // For parsing requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,23 +51,46 @@ var routes = require('./api/routes/userMgmtServiceRoutes');
 //register the route
 routes(app); 
 
-// ZooKeeper Service registration
-var zookeeper = require('node-zookeeper-client');
-var client = zookeeper.createClient('localhost:2181');
-var path = "/ProfileServices";
-var data ={host: "localhost", port: "3001"}
-client.once('connected', function () {
-    console.log('[UserMgmt] Connected to ZOOKEEPER!');
+
+
+//getting the IP
+dynamicAddress=function(){
+    axios({
+      method:'get',
+      url: 'https://ip.42.pl/raw',
+    }).then((response)=>{
+      console.log("got the IP")
+       hostAddress=response.data
+       var data={host:hostAddress,port:'3002'}
+
+        client.once('connected', function () {
+    console.log(data)
+    console.log('[APIBroker] Connected to ZOOKEEPER!');
  
     client.create(path, new Buffer(JSON.stringify(data)), function (error) {
-        if (error) {
-            console.log('[UserMgmt]  Failed to create node: %s due to: %s.', path, error);
-        } else {
-            console.log('[UserMgmt] Node: %s is successfully created.', path);
+        if (error) 
+        {
+            console.log('[APIBroker]  Failed to create node: %s due to: %s.', path, error);
+        }
+        else
+        {
+            console.log('[APIBroker]  Node: %s is successfully created.', path);
         }
  
         client.close();
     });
-});
+    });
  
 client.connect();
+
+    }).catch((err)=>{
+      res.status(400).json(utils.custom_JSON_formatter('error',err))
+    });
+    return hostAddress
+    
+}
+var temp;
+temp=dynamicAddress();
+
+
+
