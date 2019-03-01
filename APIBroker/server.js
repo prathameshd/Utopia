55259d78e5d0fa6d1745d911c02653bf94958aff
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-02-28 19:25:05 -0500

    #93: fixed conflicts

--- APIBroker/server.js
+++ APIBroker/server.js
@@ -4,14 +4,15 @@
  */
 
 // Express
-var express = require('express'),
+var express = require('express')
+var axios = require('axios')
+
   app = express(),
   port = process.env.PORT || 3002,
   bodyParser = require('body-parser');
 
-
 const cors = require('cors')
-const axios = require("axios");
+
 
 // For parsing requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,49 +34,34 @@ routes(app);
 var zookeeper = require('node-zookeeper-client');
 var client = zookeeper.createClient('149.165.170.7:2181');
 var path = "/APIBroker";
-var hostAddress=" ";
 
-//getting the IP
+//get dynamic IP
 dynamicAddress=function(){
     axios({
       method:'get',
       url: 'https://ip.42.pl/raw',
     }).then((response)=>{
-      console.log("got the IP")
-       hostAddress=response.data
-       var data={host:hostAddress,port:'3002'}
-
-        client.once('connected', function () {
-    console.log(data)
+    var data={host:response.data,port:'3002'}
+    client.once('connected', function () {
     console.log('[APIBroker] Connected to ZOOKEEPER!');
 
     client.create(path, new Buffer(JSON.stringify(data)), function (error) {
-        if (error) 
-        {
+        if (error) {
             console.log('[APIBroker]  Failed to create node: %s due to: %s.', path, error);
-        }
-        else
-        {
+        } else {
             console.log('[APIBroker]  Node: %s is successfully created.', path);
         }
 
         client.close();
     });
-<<<<<<< HEAD
-    });
- 
-=======
 });
 
->>>>>>> ecec4cf9e234458bb31f4caf9723f8865d9ad270
 client.connect();
 
     }).catch((err)=>{
-      res.status(400).json(utils.custom_JSON_formatter('error',err))
+
     });
-    return hostAddress
-    
-}
+
+  }
 var temp;
 temp=dynamicAddress();
-
