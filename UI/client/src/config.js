19899c8bd816c3cf86ceb072d8bc3aac5fb99d11
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-03-01 12:07:09 -0500

    TEST:changed ports to VM

--- UI/client/src/config.js
+++ UI/client/src/config.js
@@ -1,9 +1,9 @@
 module.exports= {
-  "authHost":"http://localhost:5000",
+  "authHost":"149.165.169.248:5000",
   "apiBrokerHost":"http://localhost:3002",
   "recommendationEngine":"http://localhost:8001/",
   "profileServices":"http://localhost:3001",
-  "apiGateway":"http://localhost:3003",
+  "apiGateway":"149.165.169.248:3003",
   "accessToken" : "",
   "name":""
 }
