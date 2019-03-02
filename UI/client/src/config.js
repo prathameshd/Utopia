441235d1a14153a3c0e19f7f78ce179c0b7e9a7e
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-03-02 14:15:23 -0500

    #98: Updated config file

--- UI/client/src/config.js
+++ UI/client/src/config.js
@@ -1,9 +1,9 @@
 module.exports= {
-  "authHost":"http://149.165.169.216:5000",
+  "authHost":"http://localhost:5000",
   "apiBrokerHost":"http://localhost:3002",
   "recommendationEngine":"http://localhost:8001/",
   "profileServices":"http://localhost:3001",
-  "apiGateway":"http://149.165.169.216:3003",
+  "apiGateway":"http://149.165.168.217:3003",
   "accessToken" : "",
   "name":""
 }
