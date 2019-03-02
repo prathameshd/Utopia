82329d38e9f3bd423d21f6c8661b86f87caa03cc
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-03-02 16:23:39 -0500

    #98: Bug fixes 3.0

--- APIBroker/api/routes/APIBrokerRoutes.js
+++ APIBroker/api/routes/APIBrokerRoutes.js
@@ -18,7 +18,5 @@ module.exports = function(app) {
   app.route('/getRecommendedTracks')
   .post(spotifyAuth.get_recommended_track)
   
-    app.route('/passIp')
-  .post(spotifyAuth.pass_ip)
     
 };
