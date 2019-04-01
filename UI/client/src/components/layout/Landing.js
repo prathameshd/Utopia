c96f68263143c6a055ed36642f16a7ecc01938d2
aparappi <aparappi@iu.edu>
2019-04-01 18:53:20 -0400

    #130: More testiing

--- UI/client/src/components/layout/Landing.js
+++ UI/client/src/components/layout/Landing.js
@@ -11,7 +11,7 @@ class Landing extends Component{
                           <div className="col-md-12 text-center">
                             <h2 className="display-3 mb-4 title" >Utopia
                             </h2>
-                            <p className="lead"> Match music to your mood!</p>
+                            <p className="lead"> Match music to your mood!!!</p>
                            
                             <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                             
